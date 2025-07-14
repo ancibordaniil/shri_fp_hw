c23b5bc7de6543a07a92fa25bc3a5b983d9c578c
Antsibor Daniil <s1lence0073777@gmail.com>
2025-07-14 15:54:13 +0300

    sexond task done

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,62 @@
-/**
- * @file Домашка по FP ч. 2
- *
- * Подсказки:
- * Метод get у инстанса Api – каррированый
- * GET / https://animals.tech/{id}
- *
- * GET / https://api.tech/numbers/base
- * params:
- * – number [Int] – число
- * – from [Int] – из какой системы счисления
- * – to [Int] – в какую систему счисления
- *
- * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
- */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import {
+  allPass,
+  gt,
+  lt,
+  length,
+  test,
+  pipe,
+  __,
+  prop,
+  mathMod,
+  andThen,
+  otherwise,
+} from 'ramda';
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const API_NUMBERS = 'https://api.tech/numbers/base';
+const API_ANIMALS = 'https://animals.tech/';
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const isLengthOk = str => gt(length(str), 2) && lt(length(str), 10);
+const isNumeric = test(/^[0-9]+\.?[0-9]*$/);
+const isPositive = x => Number(x) > 0;
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const round = x => Math.round(Number(x));
+const toBinaryParams = number => ({ from: 10, to: 2, number });
+const square = x => x ** 2;
+const mod3 = x => mathMod(x, 3);
+const getResult = prop('result');
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const validate = allPass([isLengthOk, isNumeric, isPositive]);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+  const fail = () => handleError('ValidationError');
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+  const handle = pipe(
+    Number,
+    round,
+    n => {
+      writeLog(n);
+      return api.get(API_NUMBERS, toBinaryParams(n));
+    },
+    andThen(res => {
+      const binary = getResult(res);
+      writeLog(binary);
+      const len = binary.length;
+      writeLog(len);
+      const sq = square(len);
+      writeLog(sq);
+      const mod = mod3(sq);
+      writeLog(mod);
+      return api.get(`${API_ANIMALS}/${mod}`);
+    }),
+    andThen(res => handleSuccess(getResult(res))),
+    otherwise(handleError)
+  );
+
+  writeLog(value);
+  validate(value) ? handle(value) : fail();
+};
 
 export default processSequence;
